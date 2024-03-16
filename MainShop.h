d9a1a61cc39254ef4c3e4d2dfe97c254032a63e4
BantalEmak <alexanderchristian100@gmail.com>
2024-03-16 14:36:38 +0700

    Tes

---
+++ MainShop.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+int MainSHOP(){
+    int PilihanArmourWeapon, PilihanWeapon, PilihanArmour;
+    scanf("%d", &PilihanArmourWeapon);
+    switch (PilihanArmourWeapon){
+        case 1 : 
+            scanf("PilihanWeapon");
+            break;
+        case 2 : 
+            scanf("PilihanArmour");
+            break;
+    }   
+}
